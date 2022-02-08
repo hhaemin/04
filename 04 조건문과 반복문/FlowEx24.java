3537bdffc03810fb4a33b2c83f7ca075825da2be
hhaemin <81980221+hhaemin@users.noreply.github.com>
2022-02-08 13:35:41 +0900

    자바의 정석 개정 3판 반복문 예제

---
+++ 04 조건문과 반복문/FlowEx24.java
@@ -0,0 +1,16 @@
+class FlowEx24 {
+	public static void main(String [] args) {
+		int i=11;
+		
+		System.out.println("카운트다운을 시작합니다.");
+		
+		while(i--!=0) {
+			System.out.println(i);
+			
+			for(int j=0;j<2_000_000_000;j++) {
+				;  //아무런 내용도 없는 문장 
+			}
+		}
+		System.out.println("GAME OVER");
+	}
+}
